891e62ef21761795cdbd3b97a8a6ca1815ce7dec
Michael Blank <mblank@bawue.de>
2017-08-08 07:03:18 +0200

    corrected library properties

--- EthernetBonjour3.h
+++ EthernetBonjour3.h
@@ -1,14 +1,14 @@
 //  Copyright (C) 2010 Georg Kaindl
 //  http://gkaindl.com
 //
-//  This file is part of Arduino EthernetBonjour.
+//  This file is part of Arduino EthernetBonjour3.
 //
-//  EthernetBonjour is free software: you can redistribute it and/or
+//  EthernetBonjour3 is free software: you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public License
 //  as published by the Free Software Foundation, either version 3 of
 //  the License, or (at your option) any later version.
 //
-//  EthernetBonjour is distributed in the hope that it will be useful,
+//  EthernetBonjour3 is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 EthernetBonjour3
 ================
 
-## to be used with Ethernet3 Lib
-
 Bonjour (ZeroConf) Library for Arduino & EthernetShield v2 
 
 mDNS (registering services) and DNS-SD (service discovery) has been tested and works on:
@@ -17,6 +15,4 @@ use together with
 ## Changelog
  - 06-Aug-2017 to be used with EthernetShield V2
 
- - 22-Dec-2015 Updated to provide compatibility with Arduino 1.6.6. Since 1.6.6. the Ethernet library provides support for multicast a pre-requisite for Bonjour to work.
 
- - 29-06-2019 Fixed string zero termination which breaks everything - Marcel Benning / deltacore
